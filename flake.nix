068bb0f8c604ba987307acade19aa6506ae7bd37
Austin Seipp <aseipp@pobox.com>
2023-07-27 13:14:57 -0500

    chore(nix): add basic flake scaffold
    Summary: Just the tiniest bits and doo-dads.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    Change-Id: I24c71b7b83f42d2fcf2d51b7b9f1519e

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Prototype tooling for deploying PostgreSQL";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            coreutils
+          ];
+        };
+      }
+    );
+}
