af7f05af67652b51b5b68d6eb24b9142474198ab
Austin Seipp <aseipp@pobox.com>
2023-07-31 10:48:16 -0500

    nix(postgres): add sample psql_14 and psql_15 packages
    Summary: This just adds a *very* basic demo of how to build and test PostgreSQL
    14 and PostgreSQL 15 as two separate derivations.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    Change-Id: Ia1a0f62239ad5bf89300df43eee68ee9

--- flake.nix
+++ flake.nix
@@ -10,7 +10,25 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
       in {
+        packages = {
+
+          # PostgreSQL 14 + extensions
+          psql_14 = pkgs.postgresql_14.withPackages (ps: [
+            ps.postgis
+            ps.pgtap
+            ps.pgaudit
+          ]);
+
+          # PostgreSQL 15 + extensions
+          psql_15 = pkgs.postgresql_15.withPackages (ps: [
+            ps.postgis
+            ps.pgtap
+            ps.pgaudit
+          ]);
+
+        };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             coreutils
