ebc524c3e9279c29253ae5d4f0eb4b159b62d321
Austin Seipp <aseipp@pobox.com>
2023-08-01 16:43:48 -0500

    chore: add 'nix-update' to devShell
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    Change-Id: I81fd5b80733e4de7c354e17af1613e18

--- flake.nix
+++ flake.nix
@@ -101,7 +101,7 @@
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            coreutils just
+            coreutils just nix-update
           ];
         };
       }
