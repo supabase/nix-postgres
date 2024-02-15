4e4d92e80188b7b39373a4e6179a69f132be2de3
samrose <samuel.rose@gmail.com>
2024-02-15 11:10:00 -0500

    fix: update ref

--- flake.nix
+++ flake.nix
@@ -348,7 +348,7 @@
           # set can go here.
           inherit (pkgs)
             # NOTE: comes from our cargo-pgrx.nix overlay
-            cargo-pgrx_0_11_2;
+            cargo-pgrx_0_11_0;
         };
 
         # The list of exported 'checks' that are run with every run of 'nix
